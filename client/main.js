3daa4d14277079b7d7f95b5930c7c7806e2de422
Oscar Luna <CaporalesLuna@gmail.com>
2020-06-30 12:51:10 -0700

    "Init commit: Set dev environment"

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hydrate } from 'react-dom'
-import App from './App'
+import { render } from 'react-dom'
+import HelloWorld from './HelloWorld'
 
-hydrate(<App/>, document.getElementById('root'))
+render(<HelloWorld />, document.getElementById('root'))
--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import express from 'express'
+import devBundle from './devBundle'
+import path from 'path'
+import template from './../template'
+import { MongoClient } from 'mongodb'
 
-// Connection URL
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+const url =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+MongoClient.connect(url, (err, db) => {
+  console.log('Connected successfully to mongodb server')
+  db.close()
 })
 
-app.listen(config.port, (err) => {
+const app = express()
+
+//comment out compile method when building for production
+devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+app.get('/', (req, res) => {
+  res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', config.port)
+  console.info('Server started on port %s.', port)
 })
